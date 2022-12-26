5d55518f89c7de5cced0bf2d03407f2b825cc359
Storchak Olexandr <alexsstorchak@gmail.com>
2022-12-26 21:25:05 +0200

    refactored

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.optimization.splitChunks.cacheGroups = {
+      common: {
+        name: "common",
+        chunks: "all",
+      },
+    };
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
