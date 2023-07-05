fb92e29e8f770c1675c65450490d4eff0c008c9e
Storchak Olexandr <alexsstorchak@gmail.com>
2023-07-06 02:26:48 +0300

    Updated pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -58,7 +58,7 @@ export default function App({ Component, pageProps }: AppProps) {
               name='viewport'
               content='width=device-width, initial-scale=1'
             />
-
+            <meta name="theme-color" content="#570057" />
             <link rel='icon' href='/favicon.ico' />
           </Head>
           <nav>
