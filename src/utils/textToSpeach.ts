f0a96a6dfa360fdc923ee56016a800f93bed1550
Storchak Olexandr <alexsstorchak@gmail.com>
2023-09-01 00:49:20 +0300

    Updated voice

--- src/utils/textToSpeach.ts
+++ src/utils/textToSpeach.ts
@@ -1,9 +1,8 @@
-import axios from "axios"
 export const textToSpeach = async (item: string) => {
   if ('speechSynthesis' in window) {
     const speech = new SpeechSynthesisUtterance(item)
     const voices = speechSynthesis.getVoices()
-    speech.voice = voices[50]
+    // speech.voice = voices[50]
     speech.rate = 0.8;
     speechSynthesis.speak(speech)
   } else {
