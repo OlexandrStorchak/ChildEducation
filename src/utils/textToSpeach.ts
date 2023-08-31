0ac0cfc83a9e2cc3fed067a442feeaad812f56d3
Storchak Olexandr <alexsstorchak@gmail.com>
2023-08-31 23:53:51 +0300

    Set voice by id

--- src/utils/textToSpeach.ts
+++ src/utils/textToSpeach.ts
@@ -1,12 +1,13 @@
 
-export const textToSpeach = (item: string) => {
+export const textToSpeach = async (item: string) => {
   if ('speechSynthesis' in window) {
-    const speech = new SpeechSynthesisUtterance(item);
-    const voices = speechSynthesis.getVoices();
-    speech.voice = voices[66]; //7 9 10 12 17 67
+    const speech = new SpeechSynthesisUtterance(item)
+    const voices = speechSynthesis.getVoices()
+    const voiceIndex = voices.findIndex((v) => v.voiceURI === 'Google UK English Female') //Google UK English Female
+    speech.voice = voices[voiceIndex]
     speech.rate = 0.8;
-    speechSynthesis.speak(speech);
+    speechSynthesis.speak(speech)
   } else {
-    alert('Text-to-speech is not supported in this browser.');
+    alert('Text-to-speech is not supported in this browser.')
   }
 }
