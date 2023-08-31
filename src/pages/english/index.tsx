a386ad524bb79f79bcb7051328d47c8811d479e6
Storchak Olexandr <alexsstorchak@gmail.com>
2023-08-31 23:28:58 +0300

    Added SpeechSynthesisUtterance

--- src/pages/english/index.tsx
+++ src/pages/english/index.tsx
@@ -3,8 +3,17 @@ import styles from '../../styles/Sweets.module.css'
 export default function Home() {
 
   const onItemClick = (item: string) => {
-    console.log('item - ', item)
+    if ('speechSynthesis' in window) {
+      const speech = new SpeechSynthesisUtterance(item);
+      const voices = speechSynthesis.getVoices();
+      speech.voice = voices[66]; //7 9 10 12 17 67
+      speech.rate = 0.8;
+      speechSynthesis.speak(speech);
+    } else {
+      alert('Text-to-speech is not supported in this browser.');
+    }
   }
+
   return (
     <div className={styles['sweets-wrapper']}>
       <div className={styles.separator}></div>
